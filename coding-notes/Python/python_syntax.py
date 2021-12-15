52bd80c2c4e92c19823c56c4661735885086bba8
RevengeXX <amarjithraveendran@gmail.com>
2021-12-15 17:17:37 +0530

    Create python_syntax.py

---
+++ coding-notes/Python/python_syntax.py
@@ -0,0 +1,12 @@
+
+
+## checking 
+
+if(__name__ == __main__) 
+
+what does it means ?
+  
+file is imported or not
+
+  __name__ == __main__  executed .py file is not a module
+  __name__ == __module_name__ if .py is in exported state
